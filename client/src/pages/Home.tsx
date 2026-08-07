0067549a15edef15d73dc43b14654efc2b4120d3
ssj <126043595+Samarssj@users.noreply.github.com>
2026-08-07 14:45:50 +0530

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -252,7 +252,7 @@ export default function Home() {
                     Get in Touch
                   </Button>
                 </a>
-                <a href="https://drive.google.com/file/d/1GXq-_5T2VYg4RUP9AKqnIXiZwOACUep6/view?usp=drivesdk" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1_VldnGbwaxFaPosOdnSwj6_bVUBsTGYR/view?usp=drivesdk" target="_blank" rel="noopener noreferrer">
                   <Button variant="outline" className="gap-2 hover:border-accent active:bg-accent/10 transition-all">
                     <Download className="w-4 h-4" />
                     Resume
